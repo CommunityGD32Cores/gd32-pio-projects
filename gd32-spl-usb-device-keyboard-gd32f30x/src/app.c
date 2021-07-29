be4f79d68cd5a26cdffd3b5744c6c1d4383df353
unknown <maximilian.gerhardt@rub.de>
2021-07-29 12:33:37 +0200

    Add untested USB example

---
+++ gd32-spl-usb-device-keyboard-gd32f30x/src/app.c
@@ -0,0 +1,78 @@
+/*!
+    \file    main.c
+    \brief   USB main routine for HID device(USB keyboard)
+
+    \version 2020-08-13, V3.0.0, firmware for GD32F3x0
+*/
+
+/*
+    Copyright (c) 2020, GigaDevice Semiconductor Inc. 
+
+    Redistribution and use in source and binary forms, with or without modification, 
+are permitted provided that the following conditions are met:
+
+    1. Redistributions of source code must retain the above copyright notice, this 
+       list of conditions and the following disclaimer.
+    2. Redistributions in binary form must reproduce the above copyright notice, 
+       this list of conditions and the following disclaimer in the documentation 
+       and/or other materials provided with the distribution.
+    3. Neither the name of the copyright holder nor the names of its contributors 
+       may be used to endorse or promote products derived from this software without 
+       specific prior written permission.
+
+    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED 
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. 
+IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, 
+INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT 
+NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR 
+PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, 
+WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY 
+OF SUCH DAMAGE.
+*/
+
+#include "drv_usb_hw.h"
+#include "standard_hid_core.h"
+
+extern hid_fop_handler fop_handler;
+
+usb_core_driver hid_keyboard;
+
+/*!
+    \brief      main routine will construct a USB keyboard
+    \param[in]  none
+    \param[out] none
+    \retval     none
+*/
+int main(void)
+{
+    usb_rcu_config();
+
+    usb_timer_init();
+
+    hid_itfop_register (&hid_keyboard, &fop_handler);
+
+    usbd_init (&hid_keyboard, USB_CORE_ENUM_FS, &hid_desc, &usbd_hid_cb);
+
+    usb_intr_config();
+
+#ifdef USE_IRC48M
+    /* CTC peripheral clock enable */
+    rcu_periph_clock_enable(RCU_CTC);
+
+    /* CTC configure */
+    ctc_config();
+
+    while (RESET == ctc_flag_get(CTC_FLAG_CKOK)) {
+    }
+#endif /* USE_IRC48M */
+  
+    /* check if USB device is enumerated successfully */
+    while (USBD_CONFIGURED != hid_keyboard.dev.cur_status) {
+    }
+
+    while (1) {
+        fop_handler.hid_itf_data_process(&hid_keyboard);
+    }
+}
