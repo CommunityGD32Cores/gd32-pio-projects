78fa1b04d4fa5b0dd5f843189b497cf9a7b079ee
unknown <maximilian.gerhardt@rub.de>
2022-06-04 23:57:59 +0200

    Remove non-working WIP example

--- wip/gd32-spl-usb-host-hid-custom-lib-gd323x0/include/printf_over_x.h
+++
@@ -1,7 +0,0 @@
-#ifndef PRINTF_OVER_X_H_
-#define PRINTF_OVER_X_H_
-
-/* Initializes printf transport system, e.g., the UART of semihosting service. */
-void init_printf_transport();
-
-#endif /* PRINTF_OVER_X_H_ */
