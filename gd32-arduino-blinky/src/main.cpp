7bb304c9769b4b07caa707e893f5d8ac35dff4cc
unknown <maximilian.gerhardt@rub.de>
2022-06-06 19:08:05 +0200

    Add Arduino CDC example

--- gd32-arduino-blinky/src/main.cpp
+++ gd32-arduino-blinky/src/main.cpp
@@ -1,22 +1,10 @@
 #include <Arduino.h>
 
-#ifdef LED_BUILTIN
-#define LED LED_BUILTIN
-#else 
-#define LED PC13
-//#define LED PB13
-#endif 
-
-static int i=0;
-void setup(){
+void setup() {
     Serial.begin(115200);
-    pinMode(LED, OUTPUT);
 }
-
-void loop(){
-    digitalWrite(LED, LOW);
-    delay(500);
-    digitalWrite(LED, HIGH);
-    delay(500);
-    Serial.println("Blinky nr. " + String(i++));
+int i=0;
+void loop() {
+    Serial.println("Test: " + String(i++));
+    delay(1000);
 }
