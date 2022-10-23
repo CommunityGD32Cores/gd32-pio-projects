0916022cf57a244256d5ab1de8c7dcc141eff81b
unknown <maximilian.gerhardt@rub.de>
2022-10-23 23:53:01 +0200

    Prove FreeRTOS tasks are working

--- gd32-spl-w51x-wifi/src/main.c
+++ gd32-spl-w51x-wifi/src/main.c
@@ -12,8 +12,27 @@
 #define LEDPIN      GPIO_PIN_11
 #define LED_CLOCK   RCU_GPIOB
 
-void systick_config(void);
-void delay_1ms(uint32_t count);
+#define LEDPORT     GPIOB
+#define LEDPIN      GPIO_PIN_11
+#define LED_CLOCK   RCU_GPIOB
+
+void led_task(void *p_arg) {
+    sys_reset_flag_check();
+    sys_os_misc_init();
+    systick_delay_init();
+    //wifi_management_init();
+
+    rcu_periph_clock_enable(LED_CLOCK);
+    gpio_mode_set(LEDPORT, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, LEDPIN);
+    gpio_output_options_set(LEDPORT, GPIO_OTYPE_PP, GPIO_OSPEED_2MHZ, LEDPIN);
+    while(1) {
+        gpio_bit_set(LEDPORT, LEDPIN);
+        sys_ms_sleep(1000);
+        gpio_bit_reset(LEDPORT, LEDPIN);
+        sys_ms_sleep(1000);
+        printf("Blinky!\r\n");
+    }
+}
 
 int main(void)
 {
@@ -33,6 +52,13 @@ int main(void)
     DEBUGPRINT("SDK git revision: "WIFI_GIT_REVISION" \r\n");
     DEBUGPRINT("SDK version: V%d.%d.%d\r\n", (RE_NSPE_VERSION >> 24), ((RE_NSPE_VERSION & 0xFF0000) >> 16), (RE_NSPE_VERSION & 0xFFFF));
     DEBUGPRINT("SDK build date: "BUILD_DATE" \r\n");
-    while(1) {}
+
+    sys_os_init();
+    if (NULL == sys_task_create(NULL, (const uint8_t *)"led_task", NULL, START_TASK_STK_SIZE, 0,
+            START_TASK_PRIO, led_task, NULL)) {
+        DEBUGPRINT("ERROR: create start task failed\r\n");
+    }
+    sys_os_start();
+    while(1);
     return 0;
 }
